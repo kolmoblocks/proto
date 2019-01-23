9bdb194b3da19b0ee49e8c54e556aaba7812defc
mignatovich <mikka_rus@yahoo.com>
2019-01-23 17:16:24 -0500

    Delete Exec stub implementation

--- BrowserScript/KBwasm.js
+++ BrowserScript/KBwasm.js
@@ -9,26 +9,6 @@ module.exports = class KBWasm
 
     Exec()
     {
-        // hard coded implementstion of concat buffers
-        let count = 0;
-        let result = new Array();
-
-        for ( var i in this.args )
-        {
-            let arg = this.args[i];
-            for ( var j in arg )
-            {
-                if (( j == "MIME" ) || ( j == "size" ))
-                    continue;
-
-                result.push(arg[j]);
-                count++;
-            }
-        }
-        
-        result["MIME"] = "utf8/text";
-        result["size"] = count;
-
-        return result;
+        return null;
     }
 }
