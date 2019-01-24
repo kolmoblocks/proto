154a76866ba84eadcb39207e7d336689667d4127
mignatovich <mikka_rus@yahoo.com>
2019-01-24 14:46:23 -0500

    Add first test to concat wasm project

--- WasmSrc/Concat/src/test.js
+++ WasmSrc/Concat/src/test.js
@@ -2,32 +2,44 @@ const KBstorage = require('..//..//..//BrowserScript/KBstorage')
 
 const MyStorage = new KBstorage(process.cwd() + "/../../Server/");
 
-/*
-        {
-            "exec" : {
-                "wasm" : {
-                    "ref" : "concat.wasm"
-                },
-                "arg1" : {
-                    "raw" : "123456"
-                },
-                "arg2" : {
-                    "raw" : "7890"
-                }
-            }
-        }
-*/
 
-let expression = "{\"exec\":{\"wasm\":{\"ref\":\"concat.wasm\"},\"arg1\":{\"raw\":\"123456\"},\"arg2\":{\"raw\":\"7890\"}}}";
+let val1 = "123456";
+
+let val2 = "7890";
+
+let expected_size = new Buffer(val1, 'utf8').length + new Buffer(val2, 'utf8').length;
+
+let expression = "{\"exec\":{\"wasm\":{\"ref\":\"concat.wasm\"},\"arg1\":{\"raw\":\"" + val1 + "\"},\"arg2\":{\"raw\":\"" + val2 + "\"}}}";
     
 MyStorage.GetData(expression).then( data => {
 
+    console.log("Test for data expression '" + expression + "'");
+
     if ( null == data )
-    {
-        console.log("Empty data");
-    }
+        console.error("Test failed - empty data returned!");
     else
     {
-        console.log(data);
+        let result = String.fromCharCode.apply(String, data);
+        
+        console.log("Returned data '" + result + "'");
+
+        if ( result.length == expected_size )
+        {
+            console.log("Size OK");
+
+            if ( result == (val1 + val2) )
+            {
+                console.log("Content OK");
+                console.log("Test pass OK!");
+            }
+            else
+            {
+                console.error("Different data!");
+                console.error("Exprected data '" + (val1 + val2) + "'");
+            }
+        }
+        else
+            console.error("Size of data is not equal for exprected size");   
     }
 });
+
