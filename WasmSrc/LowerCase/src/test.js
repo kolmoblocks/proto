8424ba138e838b35856cec5a6eee5fb5b309454e
mignatovich <mikka_rus@yahoo.com>
2019-01-28 14:24:29 -0500

    Fix bug in Lowercase.Wasm and refactoring tests

--- WasmSrc/LowerCase/src/test.js
+++ WasmSrc/LowerCase/src/test.js
@@ -1,45 +1,12 @@
+const common = require('..//..//common.js')
 const KBstorage = require('..//..//..//BrowserScript/KBstorage')
 const KBwasm = require('..//..//..//BrowserScript/KBwasm')
 
 const MyStorage = new KBstorage(process.cwd() + "/../../Server/");
 
-function CheckReturnedData(data, expexted_data, expected_size)
-{
-    if ( null == data )
-        console.error("Test failed - empty data returned!");
-    else
-    {
-        let result = String.fromCharCode.apply(String, data);
-
-        console.log("Returned data '" + result + "'");
-
-        if ( result.length == expected_size )
-        {
-            console.log("Size OK");
-
-            if ( result == expexted_data )
-            {
-                console.log("Content OK");
-                console.log("Test pass OK!");
-            }
-            else
-            {
-                console.error("Different data!");
-                console.error("Exprected data '" + expexted_data + "'");
-            }
-        }
-        else
-            console.error("Size of data is not equal for exprected size");   
-    }
-
-    console.log("- - - - - - - - - - - - -");
-}
-
 console.log("- - - - - - - - - - - - -");
 
-let expexted_data = "big brother";
-
-let expected_size = new Buffer(expexted_data, 'utf8').length;
+let expected_data = "big brother";
 
 {    
     let expression = "{ \"cid\" : \"FCE90620CE70369D4B6A5554CD8E52CA43B0AE303A9DE8014A5CAA88B310394D\" }";
@@ -50,7 +17,7 @@ let expected_size = new Buffer(expexted_data, 'utf8').length;
 
         console.log("Test for data expression '" + expression + "'");
 
-        CheckReturnedData(data, expexted_data, expected_size)
+        common.CheckReturnedData(data, expected_data)
         
     });
 }
@@ -72,7 +39,7 @@ let expected_size = new Buffer(expexted_data, 'utf8').length;
 
             console.log("Test 2 (direct call KBwasm)");
             
-            CheckReturnedData(data, expexted_data, expected_size);
+            common.CheckReturnedData(data, expected_data);
 
         });
     }
