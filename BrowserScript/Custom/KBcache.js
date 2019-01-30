0862158de3dbedc27b55d2c93dd3ceb486956b8c
mignatovich <mikka_rus@yahoo.com>
2019-01-30 14:27:22 -0500

    Continue with Custom

---
+++ BrowserScript/Custom/KBcache.js
@@ -0,0 +1,89 @@
+class KBcache
+{
+    constructor(hostname, port)
+    {
+        this.hostname = hostname;
+        this.port = port;
+        this.ref_cache = new Map();
+        this.cid_cache = new Map();
+    }
+
+    async requestDataFromServer(url)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", url, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+        
+        /*return new Promise((resolve, reject) => {
+            http.get(url, (res) => {
+                let data = [];
+                res.on('end', () => resolve(Buffer.concat(data)));
+                res.on('data', (chunk) => data.push(chunk));
+            }).on('error', e => reject(e));
+        });*/
+    }
+
+    async GetDataExpressionByCID(cid)
+    {
+        let cached_data = this.cid_cache.get(cid);
+
+        if ( null == cached_data )
+        {
+            let url = 'http://' + this.hostname + ':' + this.port + '/CID=' + cid;
+
+            let server_data = JSON.parse(await this.requestDataFromServer(url));
+            
+            if ( null != server_data )
+            {
+                this.cid_cache.set(cid, server_data);
+
+                return server_data;
+            }
+        }
+
+        return cached_data;
+    }
+
+    async GetRawDataByRef(ref)
+    {
+        let cached_data = this.ref_cache.get(ref);
+
+        if ( null == cached_data )
+        {
+            let url = 'http://' + this.hostname + ':' + this.port + '/REF=' + ref;
+
+            let server_data = new Uint8Array(await this.requestDataFromServer(url));
+
+            if ( null != server_data )
+            {
+                this.ref_cache.set(ref, server_data);
+                
+                return server_data;
+            }
+        }
+
+        return cached_data;
+    }
+
+    DataExpressionInCacheByCID(cid)
+    {
+        let cached_data = this.cid_cache.get(cid);
+
+        if ( null == cached_data )
+            return false;
+        else
+            return true;
+    }
+
+    RawDataInCacheByRef(ref)
+    {
+        let cached_data = this.ref_cache.get(ref);
+
+        if ( null == cached_data )
+            return false;
+        else
+            return true;
+    }
+
+}
---
+++ BrowserScript/Custom/KBwasm.js
@@ -0,0 +1,82 @@
+class KBWasm
+{
+    constructor(raw_wasm, args)
+    {
+        this.raw_wasm = raw_wasm;
+        this.args = args;
+    }
+
+    async Exec()
+    {
+        let result = new Array();
+
+        try {
+
+            var wasmModule = await new WebAssembly.Module(this.raw_wasm);
+
+            var wasmInstance = await new WebAssembly.Instance(wasmModule, []);
+
+            // fill args
+            for ( var arg in this.args )
+            {
+                if ( null == this.args[arg] )
+                {
+                    console.log("One of args is empty");
+
+                    return null;
+                }
+
+                // TODO:
+                // var arg_index = wasmInstance.exports.arg_index_by_name(args[arg]["ArgName"]);
+                var arg_index = arg;
+
+                // next string for testing WebAssembly only !
+                if ( null != this.args[arg]["ArgIndex"] ) 
+                    arg_index = this.args[arg]["ArgIndex"]
+                
+                var size = this.args[arg].length;
+
+                var pointer = wasmInstance.exports.set_arg(arg_index, size);
+
+                var pWasmData = new Uint8ClampedArray(wasmInstance.exports.memory.buffer, pointer, size);
+
+                for (var i = 0; i < pWasmData.length; i++) {
+                    pWasmData[i] = this.args[arg][i];
+                }
+            }
+
+            // exec alg in wasm
+            if ( wasmInstance.exports.exec() )
+            {
+                // get result
+                var size = wasmInstance.exports.get_result_size();
+
+                var pointer = wasmInstance.exports.get_result();
+                
+                var pResultData = new Uint8ClampedArray(wasmInstance.exports.memory.buffer, pointer, size);
+                
+                for (var i = 0; i < pResultData.length; i++) {
+                    result.push(pResultData[i]);
+                }
+
+                // TODO:
+                // result["MIME"] = wasmInstance.exports.get_result_type();
+            }
+            else
+            {
+                console.log("Assembly return false for exec");
+
+                return null;
+            }
+
+        } catch(error) {
+            
+            console.log("Error while assembly exec ",error);
+
+            return null;
+            
+        }
+
+        return result;
+    }
+}
